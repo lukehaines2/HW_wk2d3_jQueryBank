2f4c9d992bed715c2f33cda98f5424f4494744e3
LukeHaines <lukehaines2@gmail.com>
2015-08-19 19:03:23 +0100

    sudo coding HARD

--- atm-start_jQuery/atm.js
+++ atm-start_jQuery/atm.js
@@ -1,4 +1,62 @@
 $(document).ready(function(){
-  
+  console.log('ready');
 })
 
+//Get it to deposit money
+//-event listener to execute the copy of deposit value to the 'Display text'.
+//-clicking button executes the function in the event listener.
+
+variable 1 = specify where it is
+variable 2 = pull it out .text
+variable 3 = change it to number by removing the dollar sign. (use parse)
+
+function getFunds
+
+$('deposit1').on('click')
+
+getvalue = $("input1").val(); 
+console.log(getvalue);
+
+debugger;
+setValue = $('#balance2').val('bla');
+
+
+balance1 = 
+
+balance2 =  '#balance2' because its an ID
+
+var button = $('deposit1').on("click", "#filter", function() {
+    alert('clicked!');
+});
+
+
+
+
+//Get it to withdraw money
+
+
+
+//withdraw to the max value of 0
+
+
+
+
+
+//1. change box to red 
+
+
+var balanceBackground = $('balance').css("background-color","red");
+console.log(balanceBackground);
+
+
+
+//2. change box back to green
+
+// 3. Make sure numbers can't go into minus!
+// - Make a function to stop negaives
+
+
+
+
+
+//4. make current account take money from the savings account
